7f0acac4e7bb9cbb673d2483d237d97ed56e184c
CarlosEduardo12 <carlosdudujunior@hotmail.com>
2021-03-17 14:34:37 -0300

    subindo projeto

---
+++ App.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import UserContextProvider from './src/contexts/UserContext';
+
+import MainStack from './src/stacks/MainStack';
+
+export default function App() {
+  return (
+    <UserContextProvider>
+      <NavigationContainer>
+        <MainStack />
+      </NavigationContainer>
+    </UserContextProvider>
+    
+  );
+}
+
---
+++ src/screens/Preload/styles.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+export const Container = styled.SafeAreaView`
+    background-color: #FFFFFF;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`;
+export const LoadingIcon = styled.ActivityIndicator`
+    margin-top: 50px;
+`;
+
+export const TitleText = styled.Text`
+    font-size: 46px;
+    color: grey;
+    
+`;
